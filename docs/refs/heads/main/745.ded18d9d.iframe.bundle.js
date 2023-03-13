f4ba1215fe4ee325bb79097b642f63d1c7792f54
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-03-13 17:25:48 +0100

    Forgotten docs lib

---
+++ docs/refs/heads/main/745.ded18d9d.iframe.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbuilds_catalog=self.webpackChunkbuilds_catalog||[]).push([[745],{"./node_modules/react-dom/client.js":function(__unused_webpack_module,exports,__webpack_require__){var m=__webpack_require__("./node_modules/react-dom/index.js");exports.createRoot=m.createRoot,exports.hydrateRoot=m.hydrateRoot}}]);
